60acd793d3765060314d3573e0bfc4e1c611ae1b
Mattias Fredsberg <Mattias.fredsberg@cambio.se>
2023-05-16 10:25:04 +0200

    Fixed blank card bug

--- sketch.js
+++ sketch.js
@@ -3,20 +3,20 @@ let board = {
   cards: [],
   bounds: [],
   selected: [],
-  sets: []
+  sets: [],
 }
 let discard = []
 let roundTime
 let lastPick
 let player = {}
-const dimentions = {}
+const dimensions = {}
 const cardBackgrounds = {}
 const animationSpeed = 5000
-const playerColors = ['yellow', 'blue', 'green', 'red']
+const playerColors = ["yellow", "blue", "green", "red"]
 
 function setup() {
   const cnv = createCanvas(windowWidth, windowHeight)
-  cnv.style('display', 'block') // Hack to hide scrollbars https://github.com/processing/p5.js/wiki/Positioning-your-canvas#making-the-canvas-fill-the-window
+  cnv.style("display", "block") // Hack to hide scrollbars https://github.com/processing/p5.js/wiki/Positioning-your-canvas#making-the-canvas-fill-the-window
   rectMode(CENTER)
   imageMode(CENTER)
   init()
@@ -28,9 +28,13 @@ function init() {
   deck = []
 
   // Create cards
-  const x = [color('rgb(204, 102, 0)'), color('rgb(0, 204, 102)'), color('rgb(102, 0, 204)')].forEach(c => {
-    Object.keys(Fill).forEach(f => {
-      const y = ['X', 'Y', 'Z'].forEach(l => {
+  const x = [
+    color("rgb(204, 102, 0)"),
+    color("rgb(0, 204, 102)"),
+    color("rgb(102, 0, 204)"),
+  ].forEach((c) => {
+    Object.keys(Fill).forEach((f) => {
+      const y = ["X", "Y", "Z"].forEach((l) => {
         for (let n = 1; n <= 3; n++) {
           deck.push(new Card(l, n, f, c))
         }
@@ -40,12 +44,18 @@ function init() {
 
   deck = shuffle(deck)
   player.score = 0
-  player.color = color(playerColors[0])
+  calculateColor()
   updateDimensions()
   initGraphics()
 
   // for (let i = 0; i < 6; i++)
   // makeSet(deck.pop(), deck.pop(), deck.pop())
+}
+
+function calculateColor() {
+  const c = color(playerColors[0])
+  player.color = c
+  player.colorAlfa = color('rgba('+red(c)+','+ green(c)+','+ blue(c)+','+ 0.3+')')
 }
 
 function windowResized() {
@@ -55,20 +65,25 @@ function windowResized() {
 }
 
 function initGraphics() {
-  cardBackgrounds[Fill.Striped] = createCardGraphics(cardBackgrounds[Fill.Striped]) // Can prolly be precalculated
-  cardBackgrounds[Fill.Checker] = createCardGraphics(cardBackgrounds[Fill.Checker])
+  cardBackgrounds[Fill.Striped] = createCardGraphics(
+    cardBackgrounds[Fill.Striped]
+  ) // Can prolly be precalculated
+  cardBackgrounds[Fill.Checker] = createCardGraphics(
+    cardBackgrounds[Fill.Checker]
+  )
   cardBackgrounds[Fill.Wave] = createCardGraphics(cardBackgrounds[Fill.Wave]) // Can prolly be precalculated
 }
 
 function updateDimensions() {
-  dimentions.w = width * 2 / 7
-  dimentions.ws = (width - 3 * dimentions.w) / 3
-  dimentions.h = height / 6
-  dimentions.hs = (height / 10 + 3 * height / 5 + height / 10) / 4 - height / 6
-  dimentions.lineWidth = (height + width) / 100
-  dimentions.corner = (dimentions.h + dimentions.w) / 20
-  dimentions.text = min(dimentions.h / 2, dimentions.w / 3)
-  dimentions.stroke = dimentions.text / 8
+  dimensions.w = (width * 2) / 7
+  dimensions.ws = (width - 3 * dimensions.w) / 3
+  dimensions.h = height / 6
+  dimensions.hs =
+    (height / 10 + (3 * height) / 5 + height / 10) / 4 - height / 6
+  dimensions.lineWidth = (height + width) / 100
+  dimensions.corner = (dimensions.h + dimensions.w) / 20
+  dimensions.text = min(dimensions.h / 2, dimensions.w / 3)
+  dimensions.stroke = dimensions.text / 8
 }
 
 function mouseClicked(event) {
@@ -80,8 +95,11 @@ function mouseClicked(event) {
   for (let i = 0; i < board.bounds.length; i++) {
     const b = board.bounds[i]
     if (!b) continue
-    if (b.x - b.w2 < mx && mx < b.x + b.w2
-      && b.y - b.h2 < my && my < b.y + b.h2
+    if (
+      b.x - b.w2 < mx &&
+      mx < b.x + b.w2 &&
+      b.y - b.h2 < my &&
+      my < b.y + b.h2
     ) {
       result = i
       break
@@ -93,10 +111,14 @@ function mouseClicked(event) {
   }
 
   // Swap player color?
-  if (width - dimentions.w < mx && mx < width
-    && height - dimentions.h < my && my < height) {
+  if (
+    width - dimensions.w < mx &&
+    mx < width &&
+    height - dimensions.h < my &&
+    my < height
+  ) {
     playerColors.push(playerColors.shift())
-    player.color = color(playerColors[0])
+    calculateColor()
   }
 }
 
@@ -132,20 +154,20 @@ function makeSet(c1, c2, c3) {
   set.playerColor = player.color
   board.sets.push(set)
   if (board.sets.length > 6) {
-    board.sets.shift().forEach(c => discard.push())
+    board.sets.shift().forEach((c) => discard.push())
   }
 }
 
 function isSetCorrect([c1, c2, c3]) {
-  function countUniqes(a, b, c) {
+  function countUniques(a, b, c) {
     if (a == b && b == c) return 1
     if (a != b && a != c && b != c) return 3
     return 2
   }
-  const letter = countUniqes(c1.letter, c2.letter, c3.letter) != 2
-  const count = countUniqes(c1.count, c2.count, c3.count) != 2
-  const fill = countUniqes(c1.fill, c2.fill, c3.fill) != 2
-  const color = countUniqes(c1.color, c2.color, c3.color) != 2
+  const letter = countUniques(c1.letter, c2.letter, c3.letter) != 2
+  const count = countUniques(c1.count, c2.count, c3.count) != 2
+  const fill = countUniques(c1.fill, c2.fill, c3.fill) != 2
+  const color = countUniques(c1.color, c2.color, c3.color) != 2
   return letter && count && fill && color
 }
 
@@ -156,14 +178,16 @@ function pickCardFromBoard(i) {
 }
 
 function clearBoard() {
-  board.cards.forEach(c => discard.push(c))
+  board.cards.forEach((c) => {
+    if (c) discard.push(c)
+  })
   board.cards = []
   board.bounds = []
   board.selected = []
 
   if (deck.length < 12) {
     discard = shuffle(discard)
-    discard.forEach(c => deck.push(c))
+    discard.forEach((c) => deck.push(c))
     discard = []
   }
 
@@ -183,8 +207,7 @@ function draw() {
     let firstEmptySlot = board.cards.indexOf(undefined)
     if (firstEmptySlot != -1) {
       board.cards[firstEmptySlot] = deck.shift()
-    }
-    else if (board.cards.length < 12) {
+    } else if (board.cards.length < 12) {
       board.cards.push(deck.shift())
     }
     lastPick = second()
@@ -193,8 +216,12 @@ function draw() {
     }
   }
 
-  cardBackgrounds[Fill.Striped] = updateStripedBackground(cardBackgrounds[Fill.Striped])
-  cardBackgrounds[Fill.Checker] = updateCheckerBackground(cardBackgrounds[Fill.Checker])
+  cardBackgrounds[Fill.Striped] = updateStripedBackground(
+    cardBackgrounds[Fill.Striped]
+  )
+  cardBackgrounds[Fill.Checker] = updateCheckerBackground(
+    cardBackgrounds[Fill.Checker]
+  )
   cardBackgrounds[Fill.Wave] = updateWaveBackground(cardBackgrounds[Fill.Wave])
   drawBoard()
   drawSets()
@@ -202,59 +229,58 @@ function draw() {
   drawCountdown()
 }
 
-
 function drawBoard() {
-  let x = (dimentions.w + dimentions.ws) / 2
+  let x = (dimensions.w + dimensions.ws) / 2
   let y
-  const w = dimentions.w
-  const h = dimentions.h
+  const w = dimensions.w
+  const h = dimensions.h
   board.bounds = []
   board.cards.forEach((c, i) => {
     if (i % 4 == 0) {
-      y = dimentions.hs / 2 + h / 2
-      if (i > 0) x += dimentions.w + dimentions.ws
+      y = dimensions.hs / 2 + h / 2
+      if (i > 0) x += dimensions.w + dimensions.ws
     }
     if (c) {
       c.draw(x, y, w, h)
       if (board.selected.includes(i)) {
-        const c = color('rgba(204, 204, 0, 0.3)')
+        const c = player.colorAlfa
         fill(c)
         stroke(c)
-        rect(x, y, w, h, dimentions.corner)
+        rect(x, y, w, h, dimensions.corner)
       }
-      board.bounds[i] = ({ x: x, y: y, w2: w / 2, h2: h / 2 })
+      board.bounds[i] = { x: x, y: y, w2: w / 2, h2: h / 2 }
     }
-    y += dimentions.hs + h
+    y += dimensions.hs + h
   })
 }
 
 function drawSets() {
-  const w = dimentions.w
-  const h = dimentions.h
+  const w = dimensions.w
+  const h = dimensions.h
   let x = w / 2
   let y
 
   push()
-  translate(dimentions.ws / 2, 4 * (h + dimentions.hs))
+  translate(dimensions.ws / 2, 4 * (h + dimensions.hs))
   scale(1 / 3)
   rectMode(CENTER)
   board.sets.forEach((set, i) => {
     if (i % 3 == 0) {
-      y = dimentions.hs / 2 + h / 2
-      if (i > 0) x += 3 * dimentions.ws + 3 * w
+      y = dimensions.hs / 2 + h / 2
+      if (i > 0) x += 3 * dimensions.ws + 3 * w
     }
     set[0].draw(x, y)
     set[1].draw(x + w, y)
     set[2].draw(x + 2 * w, y)
     noFill()
-    strokeWeight(dimentions.stroke * 2)
+    strokeWeight(dimensions.stroke * 2)
     stroke(set.playerColor)
     if (!set.correct) {
-      fill(color('rgba(255,0,0,0.25)'))
+      fill(color("rgba(255,0,0,0.25)"))
     }
-    rect(x + w, y, w * 3, h, dimentions.corner)
+    rect(x + w, y, w * 3, h, dimensions.corner)
 
-    y += dimentions.hs + h
+    y += dimensions.hs + h
   })
   pop()
 }
@@ -264,60 +290,71 @@ function drawScore() {
   textAlign(LEFT, BOTTOM)
   const hsize = height / 25
   textSize(hsize)
-  strokeWeight(dimentions.stroke / 2)
-  let strings = [[player.score + " ", color(player.color)],
-  ["p", color('white')]]
-  drawText(width - dimentions.ws / 2 - textWidth(player.score + " p"), height - hsize - dimentions.hs / 2, strings)
+  stroke(color('black'))
+  strokeWeight(dimensions.stroke / 2)
+  let strings = [
+    [player.score + " ", color(player.color)],
+    ["p", color("white")],
+  ]
+  drawText(
+    width - dimensions.ws / 2 - textWidth(player.score + " p"),
+    height - hsize - dimensions.hs / 2,
+    strings
+  )
 }
 
 function drawText(x, y, text_array) {
-  var pos_x = x;
+  var pos_x = x
   for (var i = 0; i < text_array.length; ++i) {
-    var part = text_array[i];
-    var t = part[0];
-    var c = part[1];
-    var w = textWidth(t);
-    fill(c);
-    text(t, pos_x, y);
-    pos_x += w;
+    var part = text_array[i]
+    var t = part[0]
+    var c = part[1]
+    var w = textWidth(t)
+    fill(c)
+    text(t, pos_x, y)
+    pos_x += w
   }
 }
 function drawCountdown() {
   textStyle(NORMAL)
   textAlign(LEFT, BOTTOM)
   textSize(height / 25)
-  stroke(color('black'))
-  fill(color('white'))
-  strokeWeight(dimentions.stroke / 2)
+  stroke(color("black"))
+  fill(color("white"))
+  strokeWeight(dimensions.stroke / 2)
   const string = roundTime + " s"
-  text(string, width - dimentions.ws / 2 - textWidth(string), height - dimentions.hs / 2)
+  text(
+    string,
+    width - dimensions.ws / 2 - textWidth(string),
+    height - dimensions.hs / 2
+  )
 }
 
 const Fill = {
-  Wave: 'Wave',
-  Striped: 'Striped',
-  Checker: 'Checker'
+  Wave: "Wave",
+  Striped: "Striped",
+  Checker: "Checker",
 }
 
 function createCardGraphics(old) {
   if (old) old[0].remove()
 
-  const w = dimentions.w
-  const h = dimentions.h
+  const w = dimensions.w
+  const h = dimensions.h
 
   const cnv = createGraphics(w, h)
-  const ctx = cnv.canvas.getContext('2d')
+  const ctx = cnv.canvas.getContext("2d")
 
-  cnv.rect(0, 0, w, h, dimentions.corner)
+  cnv.rect(0, 0, w, h, dimensions.corner)
   ctx.clip()
 
   return [cnv, ctx]
 }
 
 function updateStripedBackground([cnv, ctx]) {
-  const w = dimentions.w
-  const h = dimentions.h
-  const lw = dimentions.lineWidth
+  const w = dimensions.w
+  const h = dimensions.h
+  const lw = dimensions.lineWidth
   const diag = sqrt(w * w + h * h)
 
   cnv.reset()
@@ -328,7 +365,7 @@ function updateStripedBackground([cnv, ctx]) {
   ctx.translate(-diag / 2, -diag / 2)
 
   cnv.strokeWeight(lw)
-  cnv.stroke(color('#AAA'))
+  cnv.stroke(color("#AAA"))
   const lines = diag / lw
   let y = lerp(0, 2 * lw, (millis() % animationSpeed) / animationSpeed)
   for (let i = 0; i < lines; i++) {
@@ -339,23 +376,27 @@ function updateStripedBackground([cnv, ctx]) {
 }
 
 function updateWaveBackground([cnv, ctx]) {
-  const w = dimentions.w
-  const h = dimentions.h
-  const lw = dimentions.lineWidth
+  const w = dimensions.w
+  const h = dimensions.h
+  const lw = dimensions.lineWidth
 
   cnv.reset()
   cnv.background(255)
 
-  let da = lerp(0, 2 * PI, (millis() % (2 * animationSpeed)) / (2 * animationSpeed))
+  let da = lerp(
+    0,
+    2 * PI,
+    (millis() % (2 * animationSpeed)) / (2 * animationSpeed)
+  )
   cnv.strokeWeight(lw)
-  cnv.stroke(color('#AAA'))
+  cnv.stroke(color("#AAA"))
   cnv.noFill()
   const lines = 2 + h / lw
   let y = -lw
   for (let i = 0; i < lines; i++) {
     cnv.beginShape()
     for (let x = 0; x < w; x += 5) {
-      let a = lerp(0, 2 * PI, (x + w)) / (3 * w) - da
+      let a = lerp(0, 2 * PI, x + w) / (3 * w) - da
       cnv.vertex(x, y + lw * sin(a))
     }
     cnv.endShape()
@@ -365,16 +406,17 @@ function updateWaveBackground([cnv, ctx]) {
 }
 
 function updateCheckerBackground([cnv, ctx]) {
-  const w = dimentions.w
-  const h = dimentions.h
-  const lw = dimentions.lineWidth
+  const w = dimensions.w
+  const h = dimensions.h
+  const lw = dimensions.lineWidth
 
   cnv.reset()
   cnv.background(255)
 
   cnv.noStroke()
-  cnv.fill(color('#AAA'))
-  let dr = lw * sin(lerp(0, 2 * PI, (millis() % animationSpeed) / animationSpeed))
+  cnv.fill(color("#AAA"))
+  let dr =
+    lw * sin(lerp(0, 2 * PI, (millis() % animationSpeed) / animationSpeed))
   const rows = h / (2 * lw) + 1
   const cols = w / (2 * lw) + 1
   for (let row = 0; row < rows; row++) {
@@ -400,15 +442,15 @@ class Card {
     textAlign(CENTER, CENTER)
     image(cardBackgrounds[this.fill][0], x, y)
     stroke(this.color)
-    strokeWeight(dimentions.stroke)
+    strokeWeight(dimensions.stroke)
     noFill()
-    rect(x, y, dimentions.w, dimentions.h, dimentions.corner)
+    rect(x, y, dimensions.w, dimensions.h, dimensions.corner)
 
-    textSize(dimentions.text)
+    textSize(dimensions.text)
     textStyle(BOLD)
-    stroke(color('black'))
+    stroke(color("black"))
     fill(this.color)
-    strokeWeight(dimentions.stroke)
+    strokeWeight(dimensions.stroke)
     text(this.letter.repeat(this.count), x, y + 5)
   }
 }
